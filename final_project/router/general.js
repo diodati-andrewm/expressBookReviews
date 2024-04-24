13dc25e229383d5788eea075ebd13ce3aea9b22a
Andrew Diodati <diodati-andrewm@gmail.com>
2024-04-24 16:37:19 -0400

    completed task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,8 +23,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  let bookList = [];
+  for(let i=1; i<Object.keys(books).length; i++) {
+    if(books[i].author == author) {
+        bookList.push(books[i]);
+    }
+  }
+  if(bookList.length > 0) {
+    res.send(bookList);
+  }
+  else {
+    return res.status(404).json({message: "Author not found: " + author});
+  }
+  
 });
 
 // Get all books based on title
