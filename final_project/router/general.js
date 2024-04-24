ba9d3a47f62a35781409ec1d765efbe5e1a1b760
Andrew Diodati <diodati-andrewm@gmail.com>
2024-04-24 16:45:17 -0400

    task 5 complete

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -58,8 +58,8 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(books[req.params.isbn].reviews);
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
