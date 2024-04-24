bcbd83eaaab29d4da6e13668695fa75f8f0f3c72
Andrew Diodati <diodati-andrewm@gmail.com>
2024-04-24 16:46:28 -0400

    clearing comment

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -59,7 +59,6 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   res.send(books[req.params.isbn].reviews);
-  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
